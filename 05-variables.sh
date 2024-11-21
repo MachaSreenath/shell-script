602bbf6847b912dd652a23a30fa5ccc83a09bf6f
MachaSreenath <sreenathshree9@gmail.com>
2024-11-21 16:38:27 +0530

    another change

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "enter your username"
 
-read username #the value entered above will automatically attached to username variable
+read -s username #the value entered above will automatically attached to username variable
 
-echo "username is $username"
+echo "username is $username" #this is just for referencing , do not print this statement in real scenario
