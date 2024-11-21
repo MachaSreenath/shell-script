4bd62a78a59dad72ce6b72be7cb98fc83e32bb76
MachaSreenath <sreenathshree9@gmail.com>
2024-11-21 17:49:22 +0530

    another change

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,6 +4,6 @@ FRUITS=("apple" "banana" "mango")
 
 echo "print fruits: ${FRUITS[0]}"
 
-echo "print fruits: $FRUITS[@]"
+echo "print fruits: ${FRUITS[@]}"
 
 echo "print array length : ${#FRUITS[@]}"
