8b198edcde3c842a44448e2efce43cfc37693aea
MachaSreenath <sreenathshree9@gmail.com>
2024-11-21 12:14:23 +0530

    third change

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh : hi suresh"
-echo "Suresh : hi ramesh"
-echo "Ramesh : how are you"
-echo "Suresh : i am good"
-echo "Ramesh : good"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : hi $PERSON2"
+echo "$PERSON2 : hi $PERSON1"
+echo "$PERSON1 : how are you"
+echo "$PERSON2 : i am good"
+echo "$PERSON1 : good"
