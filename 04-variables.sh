7237e5a8add69a81322a977e7b81b046939d6f62
MachaSreenath <sreenathshree9@gmail.com>
2024-11-21 12:49:30 +0530

    second change in variables another file

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this is the program where we can pass the names in the time of execution 
+# sh <filename.sh> ram rahim suresh prakash
+# it takes ram and rahim if we give 1 and 2, like that we can give no. of names
+PERSON1=
+PERSON2=
+
+echo "$PERSON1 : hi $PERSON2"
+echo "$PERSON2 : hi $PERSON1"
+echo "$PERSON1 : how are you"
+echo "$PERSON2 : i am good"
+echo "$PERSON1 : good"
