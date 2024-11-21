7bbc6226450521cb18fbd800efc9acf9f0b90ab2
MachaSreenath <sreenathshree9@gmail.com>
2024-11-21 16:42:42 +0530

    another change

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,9 +3,10 @@
 echo "enter your username"
 
 read -s username #the value entered above will automatically attached to username variable
+#here in the above line "-s" is used to hide the input from the user
 
 #echo "username is $username" #this is just for referencing , do not print this statement in real scenario
 
 echo "enter your password"
 
-read -s password
+read password
