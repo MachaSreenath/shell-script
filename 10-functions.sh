5c3cced05461d6e2e35a3b8a4af6277f77203fee
MachaSreenath <sreenathshree9@gmail.com>
2024-11-22 12:32:29 +0530

    another change

---
+++ 10-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; 
+then
+    echo "ERROR:$2....Failure"
+    exit 1
+else
+    echo "$2.....Success"
+fi
+
+}
+
+id=$(id -u)
+
+if [ $id -ne 0 ]; then
+   echo "ERROR: Please run this script as root"
+   exit 1 # you can give other than 0
+else
+   echo "you are a root user"
+fi #fi means reverse of if,indicating condition end
+
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
