4b888baca367827bf158579264f3bc2b4d53ed05
MachaSreenath <sreenathshree9@gmail.com>
2024-11-22 12:52:29 +0530

    another change

---
+++ 11-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+id=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; 
+then
+    echo "ERROR:$2....Failure"
+    exit 1
+else
+    echo "$2.....Success"
+fi
+
+}
+
+if [ $id -ne 0 ]; then
+   echo "ERROR: Please run this script as root"
+   exit 1 # you can give other than 0
+else
+   echo "you are a root user"
+fi #fi means reverse of if,indicating condition end
+
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
