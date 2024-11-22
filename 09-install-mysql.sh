6042ff027c6cba21a2715a31f71a732cb38958e0
MachaSreenath <sreenathshree9@gmail.com>
2024-11-22 11:45:14 +0530

    another change

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,7 +4,7 @@ id=$(id -u)
 
 if [ $id -ne 0 ]; then
    echo "ERROR: Please run this script as root"
-   exit 1 # you can give other than 0
+   exit 0 # you can give other than 0
 else
    echo "you are a root user"
 fi #fi means reverse of if,indicating condition end
