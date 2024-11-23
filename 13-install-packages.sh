2f1e24af5cef9d096294a61d9122a9a94d0fd89a
MachaSreenath <sreenathshree9@gmail.com>
2024-11-23 10:43:57 +0530

    another change

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,7 +5,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
+    echo -e "\e[31m ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo -e "\e[33m You are root user $N"
