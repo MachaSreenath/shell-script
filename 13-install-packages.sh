c878ca5bd79ee5dd4af4aedea94eb1b0e18c4105
MachaSreenath <sreenathshree9@gmail.com>
2024-11-23 11:13:16 +0530

    another change

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -3,6 +3,13 @@
 
 ID=$(id -u)
 
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
@@ -10,23 +17,26 @@ VALIDATE(){
     else
         echo "$2 ..... is \e[32m Success \e[0m"
     fi
-
 }
 
 if [ $ID -ne 0 ]
 then
     echo -e "\e[31m ERROR:: Please run this script with root access \e[0m"
-    exit 1 # you can give other than 0
+    exit 1 #you can give other than 0
 else
     echo -e "\e[33m You are root user \e[0m"
-fi # fi means reverse of if, indicating condition end
+fi #fi means reverse of if, indicating condition end
+
+#git postfix mysql net-tools
 
 for package in $@
 do
-    yum list installed $package
-    if [ $? -ne 0 ]
+    yum list installed $package &>> $LOGFILE    #checks package is installed or not
+    if [ $? -ne 0 ] #if not installed
     then
-        yum install $package -y
-        VALIDATE $? "Installation of $package"
-
+        yum install $package -y &>> $LOGFILE #install package
+        VALIDATE $? "Installation of $package" #validate installation
+    else
+        echo -e "$package already installed, \e[33m skipping \e[0m"
+    fi
 done
