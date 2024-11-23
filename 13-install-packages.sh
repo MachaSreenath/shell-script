b6ac94c6edd661c8dbc6a40bc7f813953ec76225
MachaSreenath <sreenathshree9@gmail.com>
2024-11-23 11:00:27 +0530

    another change

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -3,6 +3,16 @@
 
 ID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 .... is \e[31m Failed \e[0m"
+    else
+        echo "$2 ..... is \e[32m Success \e[0m"
+    fi
+
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "\e[31m ERROR:: Please run this script with root access \e[0m"
@@ -11,4 +21,12 @@ else
     echo -e "\e[33m You are root user \e[0m"
 fi # fi means reverse of if, indicating condition end
 
-echo "All arguments passed: $@"
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+
+done
