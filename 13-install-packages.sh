4416f6ab51c514750985391c66e88f51f640e7fe
MachaSreenath <sreenathshree9@gmail.com>
2024-11-23 10:45:33 +0530

    another change

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,10 +5,10 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-    echo -e "\e[31m ERROR:: Please run this script with root access $N"
+    echo -e "\e[31m ERROR:: Please run this script with root access \e[0m"
     exit 1 # you can give other than 0
 else
-    echo -e "\e[33m You are root user $N"
+    echo -e "\e[33m You are root user \e[0m"
 fi # fi means reverse of if, indicating condition end
 
 echo "All the arguments passed: $@"
