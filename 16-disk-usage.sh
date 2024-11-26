5421f3f5cada7ff9e4ed536c8d1e44921b7da6e6
MachaSreenath <sreenathshree9@gmail.com>
2024-11-26 13:26:49 +0530

    shell script changes

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -4,21 +4,18 @@ DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
-while IFS= read line 
+while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1}')
-    if [ $usage is -ge $DISK_THRESHOLD ]
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High disk usage on $partition: $usage\n" 
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" sreenathshree9@gmail.com 
+#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
 
-#this is the email id to whom we have to sent.
-
-
-#+= is used not to overright the previous message, it will append the new message.
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
