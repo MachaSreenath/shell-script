85d14a4cd81771a2841da13ff3726a2206574805
MachaSreenath <sreenathshree9@gmail.com>
2024-11-26 13:22:46 +0530

    shell script changes

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -12,11 +12,13 @@ do
     then
         message+="High disk usage on $partition: $usage\n" 
     fi
-done <<< "$DISK_USAGE"
+done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" sreenathshree9@gmail.com #tis is the email id to whom we have to sent.
+echo "$message" | mail -s "High Disk Usage" sreenathshree9@gmail.com 
+
+#this is the email id to whom we have to sent.
 
 
 #+= is used not to overright the previous message, it will append the new message.
