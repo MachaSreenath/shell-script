227ee20af9583df474a02d11f95ddb0066bdd9d3
MachaSreenath <sreenathshree9@gmail.com>
2024-11-27 17:32:41 +0530

    shell script changes

--- Practice.sh
+++ Practice.sh
@@ -3,11 +3,21 @@
 echo "enter file name"
 read file_name
 
-if [ -f "$file_name" ]; 
+# if [ -f "$file_name" ]; 
+# then
+#     wordcount=$( wc -w < "$file_name" )
+#     echo "word count is $wordcount"
+# else
+#     echo "the file $file_name is not found"
+#     exit 1
+# fi
+
+if [ ! -f $file_name ] 
 then
-    wordcount=$( wc -w < "$file_name" )
-    echo "word count is $wordcount"
-else
     echo "the file $file_name is not found"
     exit 1
+else
+    echo "the file $file_name is found"
+    wordcount=$(wc -w < "$file_name")
+    echo "...and this file contains the words inside it is: $wordcount"
 fi
